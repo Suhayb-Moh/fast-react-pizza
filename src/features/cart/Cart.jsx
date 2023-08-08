c8d9316e2b7665250e2a247d2ee08c07d0b7ed38
Suhayb Mohamed <suhaybmaxamed@gmail.com>
2023-08-08 17:53:21 +0300

    finished design

--- src/features/cart/Cart.jsx
+++ src/features/cart/Cart.jsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import LinkButton from '../../ui/LinkButton';
 import Button from '../../ui/Button';
 import CartItem from './CartItem';
@@ -31,7 +32,8 @@ function Cart() {
 
   return (
     <div className="px-4 py-3">
-      <LinkButton to="/menu"> &larr; Back to menu</LinkButton>
+      <LinkButton to="/menu">&larr; Back to menu</LinkButton>
+
       <h2 className="mt-7 text-xl font-semibold">Your cart, %NAME%</h2>
 
       <ul className="mt-3 divide-y divide-stone-200 border-b">
@@ -39,13 +41,13 @@ function Cart() {
           <CartItem item={item} key={item.key} />
         ))}
       </ul>
+
       <div className="mt-6 space-x-2">
         <Button to="/order/new" type="primary">
           Order pizzas
         </Button>
-        <Button type="secondary" className="">
-          Clear cart
-        </Button>
+
+        <Button type="secondary">Clear cart</Button>
       </div>
     </div>
   );
